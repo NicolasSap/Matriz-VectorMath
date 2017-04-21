5c0b0347bf32cbdf4cfa5f2c237685f7948653f5
Nicolas Sapaya <Nicolas Sapaya>
2017-04-21 14:06:18 -0300

    Commit Inicial

---
+++ src/unPackage/Main.java
@@ -0,0 +1,69 @@
+package unPackage;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+	public static void main(String[] args) throws CloneNotSupportedException, DistDemException {
+//		double result;
+		try {
+//			VectorMath v1 = new VectorMath("vector.in");
+//			System.out.println(v1.toString());
+//			VectorMath v2 = new VectorMath("vector2.in");
+//			System.out.println(v2.toString());
+//			VectorMath v3 = v1.sumaVectores(v2);
+//			System.out.println(v3.toString());
+//			result = v1.producto(v2);
+//			System.out.println("Producto Escalar=" + result);
+//			
+//			System.out.println("Norma Uno = " + v1.normaUno());
+//			System.out.println("Norma Dos = " + v1.normaDos());
+//			
+//			System.out.println(v1.producto(3));
+//			
+//			System.out.println(v1.equals(v2));
+//			System.out.println(v1.equals(v1));
+//			
+//			VectorMath v5 = v1.clone();
+//			System.out.println(v5.normaInfinita());
+			MatrizMath m1 = new MatrizMath("matriz.in");
+			m1.mostrarMatriz();
+			MatrizMath m2 = new MatrizMath("matriz1.in");
+			m2.mostrarMatriz();
+			MatrizMath m3 = new MatrizMath("matriz2.in");
+			m3.mostrarMatriz();
+//			m3 = m1.sumarMatriz(m2);
+//			m3.mostrarMatriz();
+//			m3 = m1.restarMatriz(m2);
+//			m3.mostrarMatriz();
+			m3 = m1.producto(m2);
+			m3.mostrarMatriz();
+			
+			MatrizMath m4 = new MatrizMath("matriz3.in");
+			VectorMath v6 = new VectorMath("vector4.in");
+			VectorMath v7 = new VectorMath("vector5.in");
+			v7 = m4.producto(v6);
+			System.out.println(v7);
+			
+			MatrizMath m5 = new MatrizMath("matriz3.in");
+			float n = 3.2f;
+			m5 = m4.producto(n);
+			m5.mostrarMatriz();
+			
+			System.out.println(m5.equals(m5));
+			System.out.println(m5.equals(m4));
+			
+			MatrizMath m6 = new MatrizMath("matriz4.in");
+			System.out.println(m6.determinante());
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+//		} catch (DistDemException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/unPackage/MatrizMath.java
@@ -0,0 +1,274 @@
+package unPackage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrizMath {
+	private int dimCol;
+	private int dimFil;
+	private double [][] matriz;
+	
+	public MatrizMath(double[][] matriz) {
+		this.matriz = matriz;
+		this.dimFil = matriz.length;
+		this.dimCol = matriz[0].length;	
+	}
+	
+	public MatrizMath(String path) throws FileNotFoundException{
+		Scanner sc = new Scanner(new File(path));
+		matriz = new double[sc.nextInt()][sc.nextInt()];
+		dimFil = matriz.length;
+		dimCol = matriz[0].length;
+		while(sc.hasNextLine())
+			matriz[sc.nextInt()][sc.nextInt()] = sc.nextDouble();
+		sc.close();
+	}
+
+	public int getDimCol() {
+		return dimCol;
+	}
+
+	public void setDimCol(int dimCol) {
+		this.dimCol = dimCol;
+	}
+
+	public int getDimFil() {
+		return dimFil;
+	}
+
+	public void setDimFil(int dimFil) {
+		this.dimFil = dimFil;
+	}
+
+	public double[][] getMatriz() {
+		return matriz;
+	}
+
+	public void setMatriz(double[][] matriz) {
+		this.matriz = matriz;
+	}
+	
+	public void mostrarMatriz() {
+		System.out.println("Matriz: ");
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.println(Arrays.toString(matriz[i]));
+		}
+	}
+
+	public MatrizMath sumarMatriz(MatrizMath m) throws DistDemException{
+		if(this.dimCol != m.dimCol || this.dimFil != m.dimFil){
+			throw new DistDemException("No pueden sumarse matrices de distintas dimensiones");
+		}
+		double [][] matResult = new double[m.dimFil][m.dimCol];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				matResult[i][j] = matriz[i][j] + m.matriz[i][j];
+			}
+		}
+		return new MatrizMath(matResult);
+	}
+	
+	public MatrizMath restarMatriz(MatrizMath m) throws DistDemException{
+		if(this.dimCol != m.dimCol || this.dimFil != m.dimFil){
+			throw new DistDemException("No pueden sumarse matrices de distintas dimensiones");
+		}
+		double [][] matResult = new double[m.dimFil][m.dimCol];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				matResult[i][j] = matriz[i][j] - m.matriz[i][j];
+			}
+		}
+		return new MatrizMath(matResult);
+	}
+	
+	public MatrizMath producto(MatrizMath m) throws DistDemException{
+		if(this.dimCol != m.dimFil){
+			throw new DistDemException("No pueden multiplicarse las matrices, dimensiones erroneas");
+		}
+		double [][] matResult = new double[dimFil][m.dimCol];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < m.matriz[0].length; j++) {
+				for (int k = 0; k < matriz[0].length; k++) {
+					matResult[i][j] += matriz[i][k] * m.matriz[k][j];
+				}
+			}
+		}
+		return new MatrizMath(matResult);
+	}
+	
+	public VectorMath producto(VectorMath v) throws DistDemException{
+		if(this.dimCol != v.getDim()){
+			throw new DistDemException("No pueden multiplicarse la matriz por el vector, dimensiones erroneas");
+		}
+		double [] vecResult = new double[dimFil];
+		double [] aux = new double[v.getDim()];
+		aux = v.getCoord();
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				vecResult[i] += matriz[i][j] * aux[j];
+			}
+		}
+		return new VectorMath(vecResult);
+	}
+	
+	public MatrizMath producto(float n){
+		double [][] matResult = new double[dimFil][dimCol];
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				matResult[i][j] = matriz[i][j] * n;
+			}
+		}
+		return new MatrizMath(matResult);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MatrizMath other = (MatrizMath) obj;
+		if (dimCol != other.dimCol)
+			return false;
+		if (dimFil != other.dimFil)
+			return false;
+		if (!Arrays.deepEquals(matriz, other.matriz))
+			return false;
+		return true;
+	}
+	
+	public double determinante()throws DistDemException{
+		if(dimCol != dimFil){
+			throw new DistDemException("No puede calcular el determinante ya que la matriz no es cuadrada"); 
+		}
+		double res = 0;
+	    if(matriz.length==2)
+	    {
+	        res=(matriz[0][0]*matriz[1][1])-(matriz[1][0]*matriz[0][1]);
+	        return res;
+	    }
+		for (int i = 0; i < matriz.length; i++) {
+			double [][] matResult = new double[dimFil-1][dimCol-1];
+			for (int j = 0; j < matriz.length; j++) {
+				 if(j!=i){
+		                for(int k=1; k<matriz.length; k++){
+		                int indice=-1;
+		                if(j<i)
+		                indice=j;
+		                else if(j>i)
+		                indice=j-1;
+		                matResult[indice][k-1]=matriz[j][k];
+		                }
+				 }
+			}
+			if(i%2 != 1){
+					res += matriz[i][0] * det(matResult);
+			}
+			else{
+				res -= matriz[i][0] * det(matResult);
+			}
+		}
+		return res;
+	}
+	
+	public double det(double[][]m){
+		double res = 0;
+	    if(m.length==2)
+	    {
+	        res=(m[0][0]*m[1][1])-(m[1][0]*m[0][1]);
+	        return res;
+	    }
+		for (int i = 0; i < m.length; i++) {
+			double [][] matResult = new double[m.length-1][m.length-1];
+			for (int j = 0; j < m.length; j++) {
+				 if(j!=i){
+		                for(int k=1; k<m.length; k++){
+		                int indice=-1;
+		                if(j<i)
+		                indice=j;
+		                else if(j>i)
+		                indice=j-1;
+		                matResult[indice][k-1]=m[j][k];
+		                }
+				 }
+			}
+			if(i%2 != 1){
+					res += m[i][0] * det(matResult);
+			}
+			else{
+				res -= m[i][0] * det(matResult);
+			}
+		}
+		return res;
+	}
+	
+	//PABLJNN AMIGOOO
+	
+	public MatrizMath invertir(){
+		double [][] invertida = new double[matriz.length][matriz.length];
+		MatrizMath mInvertida = new MatrizMath(matriz);
+		for (int o = 0; o < invertida.length; o++) {
+			invertida[o][o] = 1;
+		}
+		for (int i = 0; i < mInvertida.matriz.length; i++) {
+			if(mInvertida.matriz[i][i] == 0){
+				moverFila(mInvertida.matriz);
+			}
+			dividir(mInvertida.matriz,i,invertida);
+			cerosInferiores(mInvertida.matriz,i,invertida);
+		}
+		for (int i = 0; i <mInvertida.matriz.length; i++) {
+			cerosSuperiores(mInvertida.matriz,i,invertida);
+		}
+		mInvertida.setMatriz(invertida);
+		return mInvertida;
+		
+	}
+
+
+
+	private void cerosSuperiores(double[][] original, int i, double[][] invertida) {
+		double valor;
+		for (int j = i+1; j < original.length; j++) {
+			valor = original[i][j];
+			for (int k = 0; k < original.length; k++) {
+				original[i][k] -= valor*original[j][k];
+				invertida[i][k] -= valor*invertida[j][k];
+
+			}
+		}
+		
+	}
+
+	private void cerosInferiores(double[][] original, int i, double[][] invertida) {
+		double valor;
+		for (int j = i+1; j < original.length; j++) {
+			valor = original[j][i];
+			for (int k = 0; k < original.length; k++) {
+				original[j][k] -= valor*original[i][k];
+				invertida[j][k] -= valor*invertida[i][k];
+
+			}
+		}
+	}
+
+	private void dividir(double[][] original, int i, double[][] invertida) {
+		// Guardo el valor previo para no perderlo y aprovechar el for para también
+		// modificar la invertida
+		double valorInicial = original[i][i];
+		for (int j = 0; j < original.length; j++) {
+			original[i][j] /= valorInicial;
+			invertida[i][j] /= valorInicial;
+		}
+		
+	}
+
+	private void moverFila(double[][] original) {
+		// TODO Auto-generated method stub
+		// REVISAR POR QUE NO SE HACE OTRA REFERENICA
+	}
+}
